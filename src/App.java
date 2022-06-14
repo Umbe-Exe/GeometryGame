b363cc44beecdec6cb940b5377309b74fe7f8792
Umberto <umbe.gt@libero.it>
2022-06-14 14:37:52 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App {
 
     public static void main(String[] av) {
 
-        new Game(4, 10, 5, "test", false, true, true);
+        new Game(4, 10, 5, "test", true, true, true);
 
     }
 }
